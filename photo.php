5be52a877aa46c95a2b295f41ffae11516e92646
Leon Chen <a781282000@gmail.com>
2019-11-18 19:56:59 +0800

    照片顯示頁面

--- photo.php
+++ photo.php
@@ -3,6 +3,27 @@
 include 'lib/define.php';
 include 'lib/funcs.php';
 
+
+if (!isset($_SESSION['user'])) {
+  echo wrongInput('通訊錯誤', 'index.php');
+} elseif (!isset($_GET['id'])) {
+  header('Location: home.php?id='. $_SESSION['user']['id']);
+}
+
+if (!isset($_GET['id'])) {
+  header('Location: home.php?id='. $_SESSION['user']['id']);
+}
+
+if (!getAlbumById($_GET['id'])) {
+  header('Location: home.php?id='. $_SESSION['user']['id']);
+}
+
+foreach (getAlbumById($_GET['id']) as $album) {
+}
+
+foreach (getUserById($_GET['id']) as $user) {
+}
+
 ?>
 
 
@@ -23,7 +44,29 @@ include 'lib/funcs.php';
         echo '<img src="img/home.jpg" class="home_pic"></a>';
       ?>
       <a href="logout.php" class="header_word">登出</a>
+      <?php
+      echo '<a href="home.php?id=' . $_SESSION['user']['id'] .'" class="header_word">我的首頁</a>'
+      ?>
     </div> 
+    <?php
+    echo '<p class="title">' . $album['author'] . '.' . $album['title'] . '</p>';
+    ?>
+    <div class="photo_frame">
+      <?php
+      foreach (getPhotoById($_GET['id']) as $photos) {
+        echo '<div class="img_edge">';
+        echo '<img src="' . $photos['path'] . '" class="img">';
+        echo '</div>';
+      }
+
+      if ($album['author'] == $_SESSION['user']['name']) {
+        echo '<div class="new_photo">';
+        echo '<a href="editphoto.php?id=' . $album['id'] . '" class="new_word">+ 新增照片</a>';
+        echo '</div>';
+      }
+      ?>
+
+    </div>  
     
   </body>  
 </html>
